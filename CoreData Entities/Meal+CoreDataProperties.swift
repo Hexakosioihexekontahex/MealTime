738c1845ff13c0ccad8cc3cda2ad8100fab5c9de
Hex <wolfgunmiller@gmail.com>
2020-02-27 15:14:59 +0300

    Initial Commit

---
+++ CoreData Entities/Meal+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Meal+CoreDataProperties.swift
+//  MealTime
+//
+//  Created by Roman Melnik on 27.02.2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Meal {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Meal> {
+        return NSFetchRequest<Meal>(entityName: "Meal")
+    }
+
+    @NSManaged public var date: Date?
+    @NSManaged public var person: Person?
+
+}
---
+++ CoreData Entities/Person+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Person+CoreDataClass.swift
+//  MealTime
+//
+//  Created by Roman Melnik on 27.02.2020.
+//  Copyright © 2020 Ivan Akulov. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Person)
+public class Person: NSManagedObject {
+
+}
